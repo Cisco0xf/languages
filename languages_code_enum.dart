faf13243942d12f0b992c63593a808c58ac325b6
Mahmoud Nagy <mahmoudalshehyby@gmail.com>
2025-07-22 08:33:15 +0300

    Create languages_code_enum.dart

---
+++ languages_code_enum.dart
@@ -0,0 +1,85 @@
+// Enumeration that contains the codes of the supported languages
+
+enum LanguageCode {
+  af, // Afrikaans
+  ar, // Arabic
+  az, // Azerbaijani
+  be, // Belarusian
+  bg, // Bulgarian
+  bs, // Bosnian
+  ca, // Catalan
+  cs, // Czech
+  cy, // Welsh
+  da, // Danish
+  de, // German
+  dv, // Divehi
+  el, // Greek
+  en, // English
+  eo, // Esperanto
+  es, // Spanish
+  et, // Estonian
+  eu, // Basque
+  fa, // Persian
+  fi, // Finnish
+  fo, // Faroese
+  fr, // French
+  gl, // Galician
+  gu, // Gujarati
+  he, // Hebrew
+  hi, // Hindi
+  hr, // Croatian
+  hu, // Hungarian
+  hy, // Armenian
+  id, // Indonesian
+  it, // Italian
+  ja, // Japanese
+  ka, // Georgian
+  kk, // Kazakh
+  kn, // Kannada
+  ko, // Korean
+  kok, // Konkani
+  ky, // Kyrgyz
+  lt, // Lithuanian
+  lv, // Latvian
+  mi, // Maori
+  mk, // Macedonian
+  mn, // Mongolian
+  mr, // Marathi
+  ms, // Malay
+  mt, // Maltese
+  nb, // Norwegian Bokmål
+  nl, // Dutch
+  nn, // Norwegian Nynorsk
+  ns, // Northern Sotho
+  pa, // Punjabi
+  pl, // Polish
+  ps, // Pashto
+  pt, // Portuguese
+  qu, // Quechua
+  ro, // Romanian
+  ru, // Russian
+  sa, // Sanskrit
+  se, // Northern Sami
+  sk, // Slovak
+  sl, // Slovenian
+  sq, // Albanian
+  sr, // Serbian
+  sv, // Swedish
+  sw, // Swahili
+  syr, // Syriac
+  ta, // Tamil
+  te, // Telugu
+  th, // Thai
+  tl, // Tagalog
+  tn, // Tswana
+  tr, // Turkish
+  tt, // Tatar
+  ts, // Tsonga
+  uk, // Ukrainian
+  ur, // Urdu
+  uz, // Uzbek
+  vi, // Vietnamese
+  xh, // Xhosa
+  zh, // Chinese
+  zu, //
+}
