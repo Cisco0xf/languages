bb3d7c1b84828df2f54cb51d6834ce21205c1c13
Mahmoud Nagy <mahmoudalshehyby@gmail.com>
2025-07-22 08:50:33 +0300

    Update languages_data.dart

--- languages_data.dart
+++ languages_data.dart
@@ -1,8 +1,27 @@
 import 'package:auto_lang_field/constants/enums.dart';
-import 'package:auto_lang_field/models/language_data.dart';
 import 'package:auto_lang_field/models/languages_regexp.dart';
 
 typedef LanguageType = Map<LanguageCode, LanguageData>;
+
+class LanguageData {
+  /// The name of the language
+  final String langName;
+
+  /// The [RegExp] of the language, and this is the key
+  final RegExp langRegExp;
+
+  /// Language code and its enum with the suppored languages
+  final LanguageCode langCode;
+
+  /// Code of the language as test
+  final String isoCode;
+
+  LanguageData({
+    required this.langName,
+    required this.langCode,
+    required this.langRegExp,
+  }) : isoCode = langCode.code;
+}
 
 /// Map that contains needed data for eatch language to use it in the detection
 
